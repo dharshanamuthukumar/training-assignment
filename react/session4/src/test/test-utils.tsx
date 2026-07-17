92e57031c1ef141a307213930b9c576d4b27fd40
dharshanamuthukumar <dharshanamuthukumar.2006@gmail.com>
2026-07-17 17:18:59 +0530

    add unit testing setup: vitest, rtl, jest-dom, test-utils

---
+++ react/session4/src/test/test-utils.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from "react";
+import { render, RenderOptions } from "@testing-library/react";
+import { ThemeProvider } from "../contexts/theme-context";
+
+function AllProviders({ children }: { children: ReactNode }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
+}
+
+function customRender(ui: React.ReactElement, options?: RenderOptions) {
+  return render(ui, {
+    wrapper: AllProviders,
+    ...options,
+  });
+}
+
+export * from "@testing-library/react";
+export { customRender as render };
